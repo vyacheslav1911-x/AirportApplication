f7825b0070690e9b7c9cd7bc4ce5cd798f1db03c
vyacheslav1911-x <prog74194@gmail.com>
2025-01-21 09:45:37 +0100

    sdsd

--- AirportApp/AirportApp/Employee.cpp
+++ AirportApp/AirportApp/Employee.cpp
@@ -1,1 +1,21 @@
 #include "Employee.h"
+#include <string>
+
+Employee::Employee(
+	const std::string& id,
+	const std::string& firstName,
+	const std::string& lastName,
+	const std::string& position,
+	const int& age,
+	const std::string& email,
+	const std::string& phoneNumber,
+	const double& salary
+) : id(id),
+firstName(firstName),
+lastName(lastName),
+position(position),
+age(age),
+email(email),
+phoneNumber(phoneNumber),
+salary(salary) {
+}
--- AirportApp/AirportApp/Flight.cpp
+++ AirportApp/AirportApp/Flight.cpp
@@ -4,60 +4,44 @@
 #include <mysql_connection.h>
 #include <cppconn/statement.h>
 #include <cppconn/prepared_statement.h>
+#include "FormatDate.h"
 
 Flight::Flight(
-	string flightId,
-	optional<string> arrival,
-	optional<string> departure,
+	const string& flightId,
+	const optional<std::chrono::system_clock::time_point>& arrival,
+	const optional<std::chrono::system_clock::time_point>& departure,
 	const Plane& plane
 ) : flightId(flightId), arrival(arrival), departure(departure), plane(plane) {
 }
 
-void Flight::printFlightInfo() const {
-	cout << "Flight number: " << flightId << endl;
-
-	if (arrival.has_value()) {
-		cout << "Arrival: " << arrival.value() << endl;
-	}
-	else {
-		cout << "Arrival: Not available" << endl;
-	}
-
-	if (departure.has_value()) {
-		cout << "Departure: " << departure.value() << endl;
-	}
-	else {
-		cout << "Departure: Not available" << endl;
-	}
+void Flight::printFlightInfo(int id) const {
+	cout << "Flight number: " << id << endl;
 
 	cout << "--- Plane Information ---" << endl;
 	plane.printPlaneInformation();
 };
 
-void Flight::addFlight(const string& databaseName, const string&user, const string& password) const{
+void Flight::addFlight(const string& databaseName, const string& user, const string& password, const Plane& plane) const {
 	try
 	{
 		sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
 		unique_ptr<sql::Connection> conn(driver->connect("tcp://127.0.0.1:3306", user, password));
 		conn->setSchema(databaseName);
 
-		
-		unique_ptr<sql::PreparedStatement> pstmt(conn->prepareStatement("INSERT INTO flights (flight_id, arrival, departure, plane_name, plane_model_name, plane_color, plane_type, plane_places, plane_available_places, plane_speed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"));
-	
-		pstmt->setString(1, flightId);
-		pstmt->setString(2, arrival.value_or("NULL"));
-		pstmt->setString(3, departure.value_or("NULL"));
 
+		unique_ptr<sql::PreparedStatement> pstmt(conn->prepareStatement("INSERT INTO flights (flight_id, arrival, departure, plane_name, plane_model_name, plane_color, plane_type, plane_places, plane_available_places, plane_speed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"));
+
+		pstmt->setString(1, flightId);
+		pstmt->setDateTime(2, formatDate(arrival));
+		pstmt->setDateTime(3, formatDate(departure));
 
 		pstmt->executeUpdate();
 
 		cout << "Flight " << this->flightId << " added successfully" << endl;
-		
+
 	}
 	catch (const std::exception&)
 	{
 
 	}
 }
-
-
